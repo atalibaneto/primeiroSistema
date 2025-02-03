818d8485110b414f07e625b3e39c532bf61c233f
ataliba.acneto <ataliba.acneto@BOT00DN099W11-1>
2025-02-03 09:25:06 -0300

    Adicionar arquivos de projeto.

---
+++ primeiroSistema/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace primeiroSistema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Olá Mundo!!!");
+            Console.WriteLine(1 + 3);
+            //declaração de variáveis -> tipo de dados | nome da variável | valor
+            string nomeAluno = "Ataliba";
+            Console.WriteLine(nomeAluno);
+        }
+    }
+}
